72126671d5c8337da06e7633b2de209f48cab0ca
Ricardo Dalcomuni <161725206+Dalcomuni@users.noreply.github.com>
2024-10-25 12:07:05 -0300

    Add files via upload

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector('.titulo');
-titulo.textContent = "DALCOMUNI Nutrição";
+titulo.textContent = "Nícolas Nutrição";
 
 var pacientes = document.querySelectorAll('.paciente');
 
